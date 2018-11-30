1f7b4f3e5ab8cb609319ddb9b4b1a5b007e5b5fd
brandynbennett <brandynlbennett@gmail.com>
2018-11-30 16:02:17 -0700

    Use `state` helper to reproduce bug

--- app/components/parent/component.js
+++ app/components/parent/component.js
@@ -1,15 +1,5 @@
 import Component from '@ember/component';
-import { Store, create } from '@microstates/ember';
-import Infant from 'microstates-test/types/infant';
-import { set } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
-
-  didReceiveAttrs() {
-    let person = Store(create(Infant), nextState => {
-      return set(this, 'person', nextState);
-    });
-    set(this, 'person', person);
-  },
 });
